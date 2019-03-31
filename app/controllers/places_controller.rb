5f2b91396e9367bf61bb9b18d69c361fc3de9b73
Andy Strube <andystrube64@gmail.com>
2019-03-30 20:49:47 -0700

    Lesson 28 complete

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.order("name").page(params[:page]).per_page(1)
@@ -11,10 +11,14 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
-  
+
   def show
     @place = Place.find(params[:id])
   end 
@@ -29,16 +33,25 @@ class PlacesController < ApplicationController
 
   def update
     @place = Place.find(params[:id])
+  
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
     end
   
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+  
     @place.destroy
     redirect_to root_path
   end
