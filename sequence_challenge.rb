a89560f8214c0af0c4068d60f0bd0be88af2fce8
Andy Strube <andystrube64@gmail.com>
2019-04-18 18:26:34 -0700

    sequence challange and image blur 2

--- sequence_challenge.rb
+++ sequence_challenge.rb
@@ -1,27 +1,40 @@
 def find_missing_number(sequence)
-    sequence_array = sequence.split(' ')
-    integer_array = sequence_array.map do |element|
-      element.to_i
+  puts sequence
+    sequence_array = sequence.split(' ').collect{|i| i.to_i}.sort
+    integer_array = sequence_array.map.with_index(1) do |element, index| 
+      if element != 0
+        return index if element != index
+      end  
+      return 1 if element.to_i == 0
     end
-    
-    integer_array = integer_array.sort
-    array = []
-    for x in (1...integer_array.length + 1)
-      array.push(x)
-    end 
-    
-    array.each_with_index do |element, index|
-      if element != integer_array[index]
-        return element
-      end
-    end
-    
-    if integer_array.include? 0
-      return 1
-    end
-    
-    if array == integer_array
-      return 0
-    end
-    
+    0
   end
+
+# old version  
+# def find_missing_number(sequence)
+#     sequence_array = sequence.split(' ')
+#     integer_array = sequence_array.map do |element|
+#       element.to_i
+#     end
+    
+#     integer_array = integer_array.sort
+#     array = []
+#     for x in (1...integer_array.length + 1)
+#       array.push(x)
+#     end 
+    
+#     array.each_with_index do |element, index|
+#       if element != integer_array[index]
+#         return element
+#       end
+#     end
+    
+#     if integer_array.include? 0
+#       return 1
+#     end
+    
+#     if array == integer_array
+#       return 0
+#     end
+    
+#   end
