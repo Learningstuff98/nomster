c048b1c484a4914f52712f9dc185ab25e5118573
Andy Strube <andystrube64@gmail.com>
2019-04-24 13:03:31 -0700

    removed junk files

--- sequence_challenge.rb
+++
@@ -1,8 +0,0 @@
-def find_missing_number(sequence)
-  sequence_array = sequence.split(' ').collect{|i| i.to_i}.sort
-  integer_array = sequence_array.map.with_index(1) do |element, index| 
-    return index if element != index && element != 0
-    return 1 if element == 0
-  end
-  0
-end
